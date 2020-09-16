c504120965e1e7bb3b49442da06664aaf2d8d8ab
gallo <claudio.ale.dev@gmail.com>
2020-09-15 21:55:16 -0300

    add_server

---
+++ adlamin/router/add_server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# usage
+# ./assign_traffic network url ip
+
+add_route() {
+
+    writeUpstreamCommand="add_server.sh $2 $3"
+
+    command="sudo docker exec router_$1 sh -c '$writeUpstreamCommand'"
+
+    sudo docker exec router_$1 nginx -s reload
+
+    eval $command
+}
