de73099855d9f33269d2881778bf9f6c700d9e7d
calenicolas <>
2022-08-10 23:21:25 -0300

    fix de typo

--- lib/mainteinance/docker/enable_docker_build.sh
+++ lib/mainteinance/docker/enable_docker_build.sh
@@ -9,12 +9,12 @@ enable_docker_build() {
     #ARGS
     INTERFACE=$1
 
-    enable_dns_client $INTEFACE
-    enable_https_client $INTEFACE
+    enable_dns_client $INTERFACE
+    enable_https_client $INTERFACE
 
-    enable_dns_forward_client $INTEFACE docker0
-    enable_https_forward_client $INTEFACE docker0
-    enable_http_forward_client $INTEFACE docker0
+    enable_dns_forward_client $INTERFACE docker0
+    enable_https_forward_client $INTERFACE docker0
+    enable_http_forward_client $INTERFACE docker0
 
-    iptables -t nat -A POSTROUTING -o $INTEFACE -j MASQUERADE
+    iptables -t nat -A POSTROUTING -o $INTERFACE -j MASQUERADE
 }
