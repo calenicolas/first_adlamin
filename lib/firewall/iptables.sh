0fcc476ef7417a1723437fc16242e418ffb09019
calenicolas <>
2022-08-10 23:36:40 -0300

    fix para no explotar si no existe la regla

--- lib/firewall/iptables.sh
+++ lib/firewall/iptables.sh
@@ -53,8 +53,8 @@ disable_client(){
     PROTOCOL=$2
     PORT=$3
 
-    iptables -D ADLAMIN-INPUT -i $INTERFACE -p $PROTOCOL --sport $PORT -m state --state ESTABLISHED -j ACCEPT
-    iptables -D ADLAMIN-OUTPUT -o $INTERFACE -p $PROTOCOL --dport $PORT -m state --state NEW,ESTABLISHED -j ACCEPT
+    iptables -D ADLAMIN-INPUT -i $INTERFACE -p $PROTOCOL --sport $PORT -m state --state ESTABLISHED -j ACCEPT || true
+    iptables -D ADLAMIN-OUTPUT -o $INTERFACE -p $PROTOCOL --dport $PORT -m state --state NEW,ESTABLISHED -j ACCEPT || true
 }
 
 enable_forward_client(){
@@ -63,13 +63,13 @@ enable_forward_client(){
 }
 
 disable_forward_client(){
-    iptables -D ADLAMIN-FORWARD -i $1 -o $2 -p $3 --sport $4 -m state --state ESTABLISHED -j ACCEPT
-    iptables -D ADLAMIN-FORWARD -o $1 -i $2 -p $3 --dport $4 -m state --state NEW,ESTABLISHED -j ACCEPT
+    iptables -D ADLAMIN-FORWARD -i $1 -o $2 -p $3 --sport $4 -m state --state ESTABLISHED -j ACCEPT || true
+    iptables -D ADLAMIN-FORWARD -o $1 -i $2 -p $3 --dport $4 -m state --state NEW,ESTABLISHED -j ACCEPT || true
 }
 
 delete_forward_to_server() {
-    iptables -D ADLAMIN-FORWARD -i $1 -o $2 -s $3 -d $4 -p $5 --dport $6 -m state --state NEW,ESTABLISHED -j ACCEPT
-    iptables -D ADLAMIN-FORWARD -o $1 -i $2 -d $3 -s $4 -p $5 --sport $6 -m state --state ESTABLISHED -j ACCEPT
+    iptables -D ADLAMIN-FORWARD -i $1 -o $2 -s $3 -d $4 -p $5 --dport $6 -m state --state NEW,ESTABLISHED -j ACCEPT || true
+    iptables -D ADLAMIN-FORWARD -o $1 -i $2 -d $3 -s $4 -p $5 --sport $6 -m state --state ESTABLISHED -j ACCEPT || true
 }
 
 forward_to_server() {
@@ -80,8 +80,8 @@ forward_to_server() {
 }
 
 delete_forward_to_server_from_any() {
-    iptables -D ADLAMIN-FORWARD -i $1 -o $2 -d $3 -p $4 --dport $5 -m state --state NEW,ESTABLISHED -j ACCEPT
-    iptables -D ADLAMIN-FORWARD -o $1 -i $2 -s $3 -p $4 --sport $5 -m state --state ESTABLISHED -j ACCEPT
+    iptables -D ADLAMIN-FORWARD -i $1 -o $2 -d $3 -p $4 --dport $5 -m state --state NEW,ESTABLISHED -j ACCEPT || true
+    iptables -D ADLAMIN-FORWARD -o $1 -i $2 -s $3 -p $4 --sport $5 -m state --state ESTABLISHED -j ACCEPT || true
 }
 
 forward_to_server_from_any() {
